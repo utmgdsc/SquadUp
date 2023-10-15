f4803a7e33eb3fafe11dc95fb8513bc691a77cf6
Jaypatel1921 <jgpziv.patel@mail.utoronto.ca>
2023-10-15 19:09:41 -0400

    App runs on both android and ios

--- SquadUp/navigation/MainContainer.andriod.js
+++ SquadUp/navigation/MainContainer.andriod.js
@@ -2,6 +2,9 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+
+
+
 // Screens
 import MainScreen from './screens/MainScreen';
 import Profile from './screens/Profile';

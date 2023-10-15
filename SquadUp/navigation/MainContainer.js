0ceaf6842a5e47082371ca296d2e276844aa7aa3
Waleed Haddad <waleed.haddad@mail.utoronto.com>
2023-10-15 18:28:02 -0400

    help me

--- SquadUp/navigation/MainContainer.js
+++ SquadUp/navigation/MainContainer.js
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-
 // Screens
 import MainScreen from './screens/MainScreen';
 import Profile from './screens/Profile';
@@ -46,7 +45,7 @@ export default function MainContainer(){
                     },
 
                     tabBarActiveTintColor: '#FF8B37',
-                    tabBarInactiveTintColor: 'lightorange',
+                    tabBarInactiveTintColor: '#FF8A99',
                     labelStyle: { paddingBottom: 5, fontSize: 10 },
                     style: {padding: 5, height: 60, backgroundColor: 'orange' }
                     

4400a8beba63cb3592fd4458ec3d1cbcce56d691
Waleed Haddad <waleed.haddad@mail.utoronto.com>
2023-10-15 18:29:43 -0400

    pls god

---
+++ SquadUp/App.jsx
@@ -0,0 +1,17 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import MainContainer from './navigation/MainContainer';
+
+export default function App(){
+  return (
+    <MainContainer/>
+  );
+}
+
+

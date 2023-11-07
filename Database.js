72fa5d9f4914e878bdc9c8ecfede61f0395923b2
Waleed Haddad <waleed.haddad@mail.utoronto.com>
2023-11-06 22:29:19 -0500

    Added more Add functions

--- Database.js
+++ Database.js
@@ -1,23 +1,34 @@
-import { collection, addDoc, setDoc, doc } from "firebase/firestore"; 
+import { collection, addDoc, setDoc, doc, Timestamp } from "firebase/firestore"; 
 import { db } from './firebaseConfig';
-
-export function addCollection(){
-    // Firestore testing
-    try {
-        const docRef = addDoc(collection(db, "users"), {
-        first: "Wald",
-        last: "Haddad",
-        born: 1815
-        });
-        console.log("Document written with ID: ", docRef.id);
-    } catch (e) {
-        console.error("Error adding document: ", e);
-    }
-}
 
 export function addUser(uid, username){
     setDoc(doc(db, "users", uid), {
         name: username
+    });
+}
+
+export function addSquad(squadname){
+    addDoc(collection(db, "squads"), {
+        name: squadname
+    });
+}
+
+export function addGoal(goalname, current, target){
+    addDoc(collection(db, "goals"), {
+        name: goalname,
+        current: current,
+        target: target
+    });
+}
+
+// type can only have two values: Sports or Workout
+// DateTime must be a Timestamp object
+// Example for Dec25, 2023: addEvent("Drop-in Boxing", "Sports", Timestamp.fromDate(new Date("2023-03-25")));
+export function addEvent(eventname, type, DateTime){
+    addDoc(collection(db, "events"), {
+        name: eventname,
+        type: type,
+        DateTime: DateTime
     });
 }
 

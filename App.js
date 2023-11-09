049a3694fd476c5b72c9d4c53fe3dd743f66cc8c
Jaypatel1921 <jgpziv.patel@mail.utoronto.ca>
2023-11-09 10:53:19 -0500

    Update App.js

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import MainContainer from './navigation/MainContainer';
 import { NavigationContainer } from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Login from './navigation/Login';
-import { onAuthStateChanged } from 'firebase/auth';
+import { User, onAuthStateChanged } from 'firebase/auth';
 import { FIREBASE_AUTH } from './firebaseConfig';
 import { useState, useEffect } from 'react';
 import Register from './navigation/Register';
@@ -24,7 +24,7 @@ export default function App(){
       <Stack.Navigator initialRouteName="Login">
         {user ? (<Stack.Screen name="MainContainer" component={MainContainer} options={{ headerShown:false }} />) : 
         (<Stack.Screen name="Login" component={Login} options={{ headerShown:false }} />)}
-        {user ? (<Stack.Screen name="MainContainer1" component={MainContainer} options={{ headerShown:false }} />) :
+        {user ? (<Stack.Screen name="MainContainer" component={MainContainer} options={{ headerShown:false }} />) :
         <Stack.Screen name="Register" component={Register} options={{ headerShown: false }} />}
       </Stack.Navigator>
     </NavigationContainer>
